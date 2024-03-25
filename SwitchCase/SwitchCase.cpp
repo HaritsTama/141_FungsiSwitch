64c16f145269edfd11aa3f83808bbe29aeb4acf2
HaritsTama <nayotama1818@gmail.com>
2024-03-25 10:21:23 +0700

    menambahkan do..while kepada menu dan menulis variable lokal int pilihan

--- SwitchCase/SwitchCase.cpp
+++ SwitchCase/SwitchCase.cpp
@@ -34,15 +34,24 @@ int pembagian(int bil_1, int bil_2)
 
 int main()
 {
-	system("cls");
-	cout << "====== MENU ======" << endl;
-	cout << "1. Perjumlahan" << endl;
-	cout << "2. Pengurangan" << endl;
-	cout << "3. Perkalian" << endl;
-	cout << "4. Pembagian" << endl;
-	cout << "5. Keluar" << endl;
-	cout << "Masukan menu yang ingin di pilih (1/2/3/4/5) : ";
-	cin >> pilihan;
+	int pilihan;
 
-	
+	do
+	{
+		system("cls");
+		cout << "====== MENU ======" << endl;
+		cout << "1. Perjumlahan" << endl;
+		cout << "2. Pengurangan" << endl;
+		cout << "3. Perkalian" << endl;
+		cout << "4. Pembagian" << endl;
+		cout << "5. Keluar" << endl;
+		cout << "Masukan menu yang ingin di pilih (1/2/3/4/5) : ";
+		cin >> pilihan;
+
+		switch (pilihan)
+		{
+		case 1:
+
+		}
+	} while ();
 }
